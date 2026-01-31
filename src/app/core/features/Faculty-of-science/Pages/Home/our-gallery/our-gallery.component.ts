086984f279a708991a33fa49f36a83eedbbe462e
AmrTarek7 <amryousef04@gmail.com>
2026-01-31 11:16:37 +0200

    feat(gallery): add modal image viewer with navigation controls
    - Add Bootstrap types as a dependency for modal support
    - Implement modal display of selected gallery image with openImage method
    - Add nextImage and prevImage methods for navigating images in modal
    - Update gallery HTML to use click event for opening modal with selected image
    - Include overlay and navigation buttons inside the modal template
    - Style modal body, content, navigation buttons, and image for better UX
    - Translate gallery section title to Arabic ("معرض الصور")

--- src/app/core/features/Faculty-of-science/Pages/Home/our-gallery/our-gallery.component.ts
+++ src/app/core/features/Faculty-of-science/Pages/Home/our-gallery/our-gallery.component.ts
@@ -2,19 +2,42 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GalleryService } from '../../../Services/gallery.service';
 import { GalleryItem } from '../../../model/gallery.model';
+import { Modal } from 'bootstrap';
 
 @Component({
   selector: 'app-our-gallery',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './our-gallery.component.html',
   styleUrls: ['./our-gallery.component.css'],
 })
 export class OurGalleryComponent implements OnInit {
   galleryItems: GalleryItem[] = [];
+  selectedImage: GalleryItem | null = null;
+  selectedIndex = 0;
 
   constructor(private galleryService: GalleryService) {}
 
   ngOnInit() {
     this.galleryItems = this.galleryService.getAll();
+  }
+  openImage(item: any, index: number) {
+    this.selectedImage = item;
+    this.selectedIndex = index;
+
+    const modal = new Modal(document.getElementById('imageModal')!);
+    modal.show();
+  }
+
+  nextImage() {
+    this.selectedIndex = (this.selectedIndex + 1) % this.galleryItems.length;
+    this.selectedImage = this.galleryItems[this.selectedIndex];
+  }
+
+  prevImage() {
+    this.selectedIndex =
+      (this.selectedIndex - 1 + this.galleryItems.length) %
+      this.galleryItems.length;
+    this.selectedImage = this.galleryItems[this.selectedIndex];
   }
 }
